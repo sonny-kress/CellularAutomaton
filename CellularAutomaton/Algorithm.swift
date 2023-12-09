1d81ded06950e9f651260e12b3a5f4c856041998
Sonny Kress <sonny.kress@icloud.com>
2023-12-09 19:57:08 +1000

    Added the code

--- CellularAutomaton/Algorithm.swift
+++ CellularAutomaton/Algorithm.swift
@@ -6,3 +6,80 @@
 //
 
 import Foundation
+
+func create_world (axis_x: Int, axis_y: Int, cell_count: Int) -> ([[Bool]], [String]) {
+    var world = Array(repeating: Array(repeating: false, count: axis_x), count: axis_y)
+    var world_str = Array(repeating: "", count: axis_y)
+    
+    var cell = 0
+    repeat {
+        let x = Int.random(in: 0..<axis_y)
+        let y = Int.random(in: 0..<axis_x)
+        if (!world[x][y]) {
+            world[x][y] = true
+            cell += 1
+        }
+    } while (cell < cell_count)
+    
+    for i in 0..<axis_y {
+        for j in 0..<axis_x {
+            world_str[i].append(world[i][j] ? "." : " ")
+        }
+    }
+    
+    return (world, world_str)
+}
+
+func calc_neighbors_Moore(world: [[Bool]], axis_x: Int, axis_y: Int, i: Int, j: Int) -> Int {
+    let n1: Int = world[(axis_x + i - 1)%axis_x][(axis_x+j-1)%axis_x] ? 1 : 0
+    let n2: Int = world[(axis_x + i - 1)%axis_x][j] ? 1 : 0
+    let n3: Int = world[(axis_x + i - 1)%axis_x][(j+1)%axis_x] ? 1 : 0
+    let n4: Int = world[i][(axis_x+j-1)%axis_x] ? 1 : 0
+    let n5: Int = world[i][(j+1)%axis_x] ? 1 : 0
+    let n6: Int = world[(i + 1)%axis_x][(axis_x+j-1)%axis_x] ? 1 : 0
+    let n7: Int = world[(i + 1)%axis_x][j] ? 1 : 0
+    let n8: Int = world[(i + 1)%axis_x][(j+1)%axis_x] ? 1 : 0
+    
+    return n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8
+}
+
+//func calc_neighbors_Neumann(world: [[Bool]], axis_x: Int, axis_y: Int, i: Int, j: Int) {
+//    return world[(axis_x + i - 1)%axis_x][j] + world[i][(axis_x+j-1)%axis_x] +
+//           world[i][(j+1)%axis_x] + world[(i + 1)%axis_x][j]
+//}
+
+func sum_cell(world: [[Bool]], axis_x: Int, axis_y: Int) -> Int {
+    var sum: Int = 0
+    for i in 0..<axis_x {
+        for j in 0..<axis_y {
+            sum += world[i][j] ? 1 : 0
+        }
+    }
+    return sum
+}
+
+func change_world (world: [[Bool]], axis_x: Int, axis_y: Int) -> ([[Bool]], [String]) {
+    var mutateWorld = world
+    var mutateWorld_str = Array(repeating: "", count: axis_y)
+    
+    for i in 0..<axis_x {
+        for j in 0..<axis_y {
+            let sum_neighbors = calc_neighbors_Moore(world: world, axis_x: axis_x, axis_y: axis_y, i: i, j: j)
+            if ((!world[i][j] && sum_neighbors == 3) || (world[i][j] && (sum_neighbors == 2 || sum_neighbors == 3))) {
+                mutateWorld[i][j] = true;
+            }
+            else {
+                mutateWorld[i][j] = false;
+            }
+        }
+    }
+    
+    for i in 0..<axis_y {
+        for j in 0..<axis_x {
+            mutateWorld_str[i].append(mutateWorld[i][j] ? "." : " ")
+        }
+    }
+    
+    
+    return (mutateWorld, mutateWorld_str)
+}
--- CellularAutomaton/ContentView.swift
+++ CellularAutomaton/ContentView.swift
@@ -7,17 +7,121 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+//    @State var axis_x: Int = 255
+//    @State var axis_y: Int = 255
+//    @State var cells_count: Int = 15000
+    @State private var isError = false
+    @State private var showWorld = false
+    @State private var start_isDisabled = false
+    @State private var stop_isDisabled = true
+    @ObservedObject var worldModel = WorldViewModel(x: 255, y: 255, c: 2000, world: ([[]], []))
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack () {
+            if (self.showWorld) {
+                GeometryReader { geo in
+                    ScrollView (.vertical) {
+                        ScrollView (.horizontal) {
+                            VStack (spacing: 0){
+                                ForEach (0..<worldModel.y, id: \.self) { i in
+                                    Text(worldModel.world.1[i]).frame(height: 2).kerning(-1)
+                                }
+                            }
+                            .frame(
+                                minWidth: geo.size.width,
+                                minHeight: geo.size.height
+                            )
+                        }
+                    }
+                }
+            }
+            
+            
+            VStack {
+                Text("Input data")
+                VStack {
+                    HStack {
+                        Text("x: ")
+                        TextField("axis x", value: $worldModel.x, formatter: NumberFormatter())
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    
+                    HStack {
+                        Text("y: ")
+                        TextField("axis y", value: $worldModel.y, format: .number)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    
+                    HStack {
+                        Text("cells: ")
+                        TextField("Cells count", value: $worldModel.c, format: .number)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    
+                }.frame(width: 150)
+                
+            
+                Button("Start") {
+//                    if (self.axis_x == 0 || self.axis_y == 0 || self.cells_count == 0) {
+//                        self.isError = true
+//                    }
+//                    else {
+                        worldModel.newWorld()
+                        self.showWorld = true
+                        start_isDisabled = true
+                        stop_isDisabled = false
+//                    }
+                }.alert(isPresented: $isError, content: {
+                    Alert(title: Text("One of the input fields is empty!"))
+                })
+                .disabled(stop_isDisabled ? false : true)
+                
+                Button("Stop") {
+                    worldModel.newWorld()
+                    stop_isDisabled = true
+                    showWorld = false
+                }.disabled(start_isDisabled ? false : true)
+                
+                Button {
+                    worldModel.updateWorld()
+                } label: {
+                    Text("Update")
+                }
+            }
+            .padding(5)
+            .frame(maxWidth: 160, alignment: .trailing)
         }
-        .padding()
+        .frame(minWidth: 600, minHeight: 400)
     }
 }
+
+class WorldViewModel: ObservableObject {
+    @Published var x: Int = 255
+    @Published var y: Int = 255
+    @Published var c: Int = 2000
+    @Published var world: ([[Bool]], [String]) = ([[]], [])
+    init(x: Int, y: Int, c: Int, world: ([[Bool]], [String])) {
+        self.x = x
+        self.y = y
+        self.c = c
+        self.world = world
+    }
+    
+    
+    func updateWorld() {
+        DispatchQueue.main.asyncAfter(
+            deadline: DispatchTime.now() + Double(Int64(0.5 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC), execute: {
+                self.world = change_world(world: self.world.0, axis_x: self.x, axis_y: self.y)
+            })
+    }
+    
+    func newWorld() {
+        self.world = create_world(axis_x: self.x, axis_y: self.y, cell_count: self.c)
+    }
+}
+
 
 #Preview {
     ContentView()

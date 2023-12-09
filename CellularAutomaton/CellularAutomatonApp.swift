ac9ae0c0a40de2bcae2ab4cb79f528659fb1d275
Sonny Kress <sonny.kress@icloud.com>
2023-12-09 19:37:05 +1000

    Initial Commit

---
+++ CellularAutomaton/CellularAutomatonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CellularAutomatonApp.swift
+//  CellularAutomaton
+//
+//  Created by Sonny Kress on 09.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CellularAutomatonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
